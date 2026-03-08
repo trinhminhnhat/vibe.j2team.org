4b18c652ea0ba3766d5cbe139c13843db6325e82
Lê Hiếu Hòa <hieuhoa.dev.cf@gmail.com>
2026-03-08 15:20:59 +0700

    fix: meta data (#79)

--- src/views/random-avatar/meta.ts
+++ src/views/random-avatar/meta.ts
@@ -1,9 +1,10 @@
 import type { PageMeta } from '@/types/page'
 
 const meta: PageMeta = {
-  name: 'Random Avatar',
-  description: 'Random Avatar Generator',
-  author: 'hieuhoa-dev',
+  name: "Random Avatar",
+  description: "Tạo avatar ngẫu nhiên với nhiều phong cách khác nhau.",
+  author: "hieuhoa-dev",
+  facebook: "https://github.com/hieuhoa-dev",
 }
 
 export default meta
